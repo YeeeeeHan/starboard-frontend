9212a70783808ecd7050cb0eafc14f87f42f6fab
Lim Yee Han <limyeehan@gmail.com>
2023-04-24 02:52:24 +0800

    header

--- src/components/modules/Header/Header.tsx
+++ src/components/modules/Header/Header.tsx
@@ -1,13 +1,12 @@
 import { Box, Container, Flex, HStack } from '@chakra-ui/react';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
-import { ColorModeButton, MoralisLogo, NavBar } from 'components/elements';
+import { ColorModeButton, NavBar } from 'components/elements';
 
 const Header = () => {
   return (
     <Box borderBottom="1px" borderBottomColor="chakra-border-color">
       <Container maxW="container.xl" p={'10px'}>
         <Flex align="center" justify="space-between">
-          <MoralisLogo />
           <NavBar />
           <HStack gap={'10px'}>
             <ConnectButton />
